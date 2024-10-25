df47784c9611266caabaed0a8a87d9301343b0c5
VishnuReddy <rvishnuvardhan028@gmail.com>
2024-10-25 15:24:41 +0530

    special constructor added at C

--- src/B.java
+++ src/B.java
@@ -3,5 +3,6 @@ public B(){
     System.out.println("B");
    // "add to the hub"
     System.out.println("B is the second constructor");
+
 }
 }
--- src/C.java
+++ src/C.java
@@ -2,4 +2,10 @@ public class C {
     public C(){
         System.out.println("C");
     }
+    String c;
+    int a;
+    public C(String c,int a){
+    this.c=c;
+    this.a=a;
+    }
 }
